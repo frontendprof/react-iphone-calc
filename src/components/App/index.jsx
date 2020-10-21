771bbba71f6fc031162d653d238cb79a621e3a58
frontendprof <abdumaliksharipov@gmail.com>
2020-10-21 11:15:22 +0500

    Styling calculator

---
+++ src/components/App/index.jsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+
+import "./App.css";
+import Button from "../Button";
+
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="top">16:44</div>
+            <div className="display">0</div>
+            <div className="buttons">
+                <Button content="AC" type="function"/>
+                <Button content="±"  type="function"/>
+                <Button content="%"  type="function"/>
+                <Button content="÷" type="operator"/>
+                <Button content="7"/>
+                <Button content="8"/>
+                <Button content="9"/>
+                <Button content="×" type="operator"/>
+                <Button content="4"/>
+                <Button content="5"/>
+                <Button content="6"/>
+                <Button content="−" type="operator"/>
+                <Button content="1"/>
+                <Button content="2"/>
+                <Button content="3"/>
+                <Button content="+" type="operator"/>
+                <Button content="0"/>
+                <Button content="."/>
+                <Button content="=" type="operator"/>
+            </div>
+            <div className="bottom"></div>
+        </div>
+    )
+}
+
+export default App;
